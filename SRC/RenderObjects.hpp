b1dd26fcac5ddf46f251e1fae5c8dca316aa8443
mandarkamble2512-debug <mandarkamble2512@gmail.com>
2026-09-21 21:09:53 +0530

    Day 26 Optimised DrawScreenGrid() function that drew background grid

--- SRC/RenderObjects.hpp
+++ SRC/RenderObjects.hpp
@@ -1,10 +1,20 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/System/Vector2.hpp>
 #include "StructsClassesEnums.hpp"
 
 using sf::RenderWindow;
 
+namespace GreenSqures 
+{
+    DarkGreenSqure SqureDark(Vector2f(0, 0));
+    LightGreenSqure SqureLight(Vector2f(0, 0));
+}
+
 void DrawScreenGrid (RenderWindow& window)
 {
+    using GreenSqures::SqureDark;
+    using GreenSqures::SqureLight;
+
     bool IsLightGreenTileDrawn = true;
     for (short X = 0; X <= 640; X = X + 32)
     {
@@ -12,14 +22,14 @@ void DrawScreenGrid (RenderWindow& window)
         {
             if (IsLightGreenTileDrawn)
             {
-                DarkGreenSqure squre(Vector2f(X, Y));
-                window.draw(squre.GreenSqure); 
+                window.draw(SqureDark.GreenSqure);
+                SqureDark.GreenSqure.setPosition(Vector2f(X, Y)); 
                 IsLightGreenTileDrawn = false;
             }
             else
             {
-                LightGreenSqure squre(Vector2f(X, Y));
-                window.draw(squre.GreenSqure);
+                SqureLight.GreenSqure.setPosition(Vector2f(X, Y));
+                window.draw(SqureLight.GreenSqure);
                 IsLightGreenTileDrawn = true;
             }
         }  
